44b360e3b04eb529323a21ad6880d97d37f58eba
SILVIAMCG <silviatapia.mcg@gmail.com>
2026-09-16 14:01:36 -0300

    actualizar configuracion

--- api/conect.py
+++ api/conect.py
@@ -1,8 +1,11 @@
 import os
 import bcchapi
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-siete = bcchapi.Siete(token=os.getenv("API_TOKEN"))
+api_token = st.secrets.get("API_TOKEN", os.getenv("API_TOKEN"))
+siete = bcchapi.Siete(token=api_token)
+
 
